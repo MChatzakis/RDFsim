51cc2bf06eac261c48d3f2daac4e25dad3b6cc75
Manos Chatzakis <chatzakis@ics.forth.gr>
2021-07-10 02:33:50 +0300

    minors

--- RDFsim/src/main/java/scripts/Examples.java
+++ RDFsim/src/main/java/scripts/Examples.java
@@ -27,14 +27,14 @@ public class Examples {
         String ariadneQuery = "select  * where {?s ?p ?o .}";
 
         SPARQLQuery sq = new SPARQLQuery();
-        String vocab = sq.getData(dbPediaEndpoint, dbPediaQuery, 20000, 0);
+        String vocab = sq.getData(dbPediaEndpoint, ariadneQuery, 50000, 0);
 
         String path = CommonUtils.writeStringToFile(vocab, "triples/example.rdf");
 
         Word2VecEmbeddingCreator vects = new Word2VecEmbeddingCreator(5, 100, 42, 5, path);
         vects.train();
 
-        String entity = "http://dbpedia.org/resource/Cassius_Longinus_(philosopher)";
+        /*String entity = "http://dbpedia.org/resource/Cassius_Longinus_(philosopher)";
         Collection<String> similars = vects.getSimilarEntities(entity, 5);
         System.out.println("Similars of " + entity + " " + similars);
 
@@ -45,7 +45,7 @@ public class Examples {
         CommonUtils.printEntityMap(topEntitiesOfPhilosopher);
 
         sim = vects.calculateCosineSimilarity("http://dbpedia.org/resource/Democritus", "http://dbpedia.org/resource/Gorgias");
-        System.out.println("Similarity (cosine): " + sim);
+        System.out.println("Similarity (cosine): " + sim);*/
     }
 
     public static void loadPreSaved() {
@@ -92,17 +92,24 @@ public class Examples {
         String vocab = sq.getData(dbPediaEndpoint, dbPediaQuery, 10, 0);
 
         //System.out.println(vocab);
-
         String path = CommonUtils.writeStringToFile(vocab, "triples/example.rdf");
         Word2VecEmbeddingCreator vects = new Word2VecEmbeddingCreator(5, 100, 42, 5, path);
         vects.train();
     }
 
     public static void main(String[] args) throws IOException {
-        //classicExample();
+        long start = System.currentTimeMillis();
+
+        classicExample();
         //loadPreSaved();
         //createDBpediaSample();
-        createAriadneSample();
+        //createAriadneSample();
         //createBiggerSequences();
+
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+
+        System.out.println("Time elapsed: " + (double) timeElapsed / 1000.0 + " seconds");
     }
+
 }
