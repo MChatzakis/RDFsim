28b39f5ad1b136f3ada52495c9543293177e94d6
Manos Chatzakis <chatzakis@ics.forth.gr>
2021-07-27 15:56:46 +0300

    added session oriented responses

--- RDFsim/src/main/java/servlets/SearchServlet.java
+++ RDFsim/src/main/java/servlets/SearchServlet.java
@@ -1,30 +1,10 @@
 package servlets;
 
-import embeddings.W2VApi;
 import java.io.File;
 import java.io.FileNotFoundException;
-import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
-import org.deeplearning4j.models.word2vec.Word2Vec;
-import org.deeplearning4j.text.sentenceiterator.BasicLineIterator;
-import org.deeplearning4j.text.sentenceiterator.LineSentenceIterator;
-import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
-import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
-import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
-import org.slf4j.impl.StaticLoggerBinder;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -45,41 +25,35 @@ import utils.CommonUtils;
  */
 @WebServlet(name = "SearchServlet", urlPatterns = {"/SearchServet"})
 public class SearchServlet extends HttpServlet {
-
+    
     String currentPrefix = "http://dbpedia.org/resource/";
     String endpoint = "https://dbpedia.org/sparql";
-    String currentInformationService = "wikipedia";
-    String currentEntity = "";
-
-    int similarsNum = 10;
-    int graphDepth = 1;
-
+    
     final int DEFAULT_SIMILARS = 10;
     final int DEFAULT_DEPTH = 1;
-
+    
     RafApi raf = null;
-    SimilarityGraph simg = null;
-
+    
     public SearchServlet() {
         super();
-
+        
         try {
             initRaf();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
+    
     private void initRaf() throws FileNotFoundException, IOException {
         String[] samples = {"philosophers", "movies", "programming_langs", "game_consoles"};
         String name = samples[0];
-
+        
         String linuxPath = "/var/lib/tomcat9/work/rdfsim/rafs/" + name + ".txt";
         String windowsPath = "C:\\tmp\\rdfsim\\rafs\\" + name + ".txt";
-
+        
         File lin = new File(linuxPath);
         File win = new File(windowsPath);
-
+        
         if (lin.exists()) {
             raf = new RafApi(linuxPath, linuxPath.replace(".txt", "PTR.txt"));
         } else if (win.exists()) {
@@ -87,104 +61,112 @@ public class SearchServlet extends HttpServlet {
         } else {
             throw new FileNotFoundException("Could not locate the raf in current file system");
         }
-
-        simg = new SimilarityGraph(raf);
+        
     }
-
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        HttpSession session = request.getSession();
         
+        HttpSession session = request.getSession();
+        String s;
+        /*Connection attributes*/
+        String currentEntity = String.valueOf(session.getAttribute("entity"));
+        Integer currentCount = ((s = String.valueOf(session.getAttribute("count"))).equals("null")) ? null : Integer.parseInt(s);        // Integer.parseInt(String.valueOf(session.getAttribute("count")));
+        Integer currentDepth = ((s = String.valueOf(session.getAttribute("depth"))).equals("null")) ? null : Integer.parseInt(s);
+        String currentService = String.valueOf(session.getAttribute("service"));
+
+        /*Attributes sent from the client forms*/
         String entity = request.getParameter("entity");
         String count = request.getParameter("count");
         String depth = request.getParameter("depth");
         String infoService = request.getParameter("info-service");
-        String rafFilePath = request.getParameter("raf-filepath");
-
-        if (rafFilePath != null) { //to compile..
-            raf = new RafApi(rafFilePath, rafFilePath.replace(".txt", "PTR.txt"));
-            simg = new SimilarityGraph(raf);
-        }
-
-        //Consider entity as URI (with prefix :) )
+        
         if (entity != null) {
-            //currentEntity = entity;
-            String[] conts = raf.getEntityContents(entity);
-            String curEn = conts[0];
+            String[] conts = (raf.getEntityContents(entity));
             String curEnURI = conts[1];
-
-            //currentEntity = curEnURI;
-            session.setAttribute("entity", curEnURI);
+            currentEntity = curEnURI;
+        } else if (currentEntity == null) {
+            currentEntity = "";
         }
-
+        
         if (count != null) {
             if (CommonUtils.isNumeric(count)) {
-                //similarsNum = (int) Double.parseDouble(count);
-                //System.out.println("Count set: " + similarsNum);
-                session.setAttribute("count", count);
+                currentCount = Integer.parseInt(count);
             }
+        } else if (currentCount == null) {
+            currentCount = DEFAULT_SIMILARS;
         }
-
+        
         if (depth != null) {
             if (CommonUtils.isNumeric(depth)) {
-                // graphDepth = (int) Double.parseDouble(depth);
-                //System.out.println("Depth set: " + graphDepth);
-                session.setAttribute("depth", depth);
+                currentDepth = Integer.parseInt(depth);
             }
+        } else if (currentDepth == null) {
+            currentDepth = DEFAULT_DEPTH;
         }
-
+        
         if (infoService != null) {
-            if (infoService.equals("wikipedia")) {
-                //currentInformationService = "wikipedia";
-                //System.out.println("Wikipedia service selected.");
-                session.setAttribute("service", "wikipedia");
+            /*if (infoService.equals("wikipedia")) {
+                //session.setAttribute("service", "wikipedia");
+                currentService = "wikipedia";
             } else if (infoService.equals("dbpedia")) {
-                //currentInformationService = "dbpedia";
-                //System.out.println("DBpedia service selected.");
-                session.setAttribute("service", "dbpedia");
+                // session.setAttribute("service", "dbpedia");
+                currentService = "dbpedia";
             } else if (infoService.equals("triples")) {
-                JSONArray jtriples = SPARQLQuery.getTriplesOfURI(currentEntity, endpoint);
-                //currentInformationService = jtriples.toString();
-                //System.out.println("Triple Services Selected");
-                session.setAttribute("service", jtriples.toString());
-            }
+                //JSONArray jtriples = SPARQLQuery.getTriplesOfURI((String) session.getAttribute("entity"), endpoint);
+                //session.setAttribute("service", jtriples.toString());
+                currentService = "triples";
+            }*/
+            currentService = infoService;
+        } else if (currentService == null) {
+            //session.setAttribute("service", "wikipedia");
+            currentService = "wikipedia";
         }
-
-        JSONObject graph2sent = new JSONObject();
-        if (simg != null) {
-            simg.createGraphRaf(currentEntity, graphDepth, similarsNum);
-            graph2sent = simg.toJSON();
-        }
-
+        
+        JSONObject graph2sent = null;
+        SimilarityGraph g = new SimilarityGraph(raf);
+        g.createGraphRaf(currentEntity, currentDepth, currentCount);
+        graph2sent = g.toJSON();
+        
         request.setAttribute("graph", graph2sent.toString());
         request.setAttribute("self", currentEntity);
-        request.setAttribute("count", similarsNum);
-        request.setAttribute("depth", graphDepth);
-        request.setAttribute("info-service", currentInformationService);
+        request.setAttribute("count", currentCount);
+        request.setAttribute("depth", currentDepth);
+        
+        if (currentService.equals("triples")) {
+            JSONArray jtriples = SPARQLQuery.getTriplesOfURI(currentEntity, endpoint);
+            request.setAttribute("info-service", jtriples.toString());
+        } else {
+            request.setAttribute("info-service", currentService);
+        }
+        
+        session.setAttribute("entity", currentEntity);
+        session.setAttribute("count", currentCount);
+        session.setAttribute("depth", currentDepth);
+        session.setAttribute("service", currentService);
 
         //System.out.println("Server connection attribute--graph: " + graph2sent.toString(2));
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("/search.jsp");
         requestDispatcher.forward(request, response);
     }
-
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        
         int type = Integer.parseInt(request.getParameter("type"));
-
+        
         PrintWriter out = response.getWriter();
-
+        
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-
+        
         int count = 0;
         JSONObject data2sent = null;
-
+        
         System.out.println("Server->Sending: " + data2sent.toString(2));
-
+        
         out.print(data2sent);
         out.flush();
     }
-
+    
 }
